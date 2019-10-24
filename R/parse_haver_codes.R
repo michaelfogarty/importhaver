d97092a16ed9b64b2f4fe688e671c0d802690a27
Michael Fogarty <mfogarty@frbchi.org>
2019-10-24 11:11:12 -0500

    Initial commit of working repo

--- R/parse_haver_codes.R
+++ R/parse_haver_codes.R
@@ -1,58 +1,43 @@
-#' Wrapper function around \code{Haver::haver.data} that returns a tibble with the dates in Date format and each series as a column.
-#' 
-#' @param series The series (in database:series or SERIES@DATABASE format) that you wish to pull from Haver.
-#' @return A character vector of Haver codes in the format database:
+#' This function parses haver codes of the format
+#' code{SERIES@DATABASE} and converts them to the format \code{database:series}
+#'
+#' @param series A series name to parse into the proper format
+#' @return A character vector of Haver codes in the format "database:series"
 #' @export
 #' @examples
-#' parse_haver_codes('GDP@USECON')
+#' parse_haver_codes("GDP@USECON")
 
-## parse_haver_codes.R
-## Author: Michael Fogarty
-## Crated: 7/15/19
-## Last Major Revision: 7/15/19
-## R Version 3.5.3
+parse_haver_codes <- function(series) {
 
-## This function parses haver codes of the format `SERIES@DATABASE` and converts
-## them to the format `database:series` for use in the `Haver::haver.data` 
-## function.
-
-parse_haver_codes <- function(series){
-  
-  ## require namespace magrittr for the pipe operator
-  requireNamespace('magrittr')
-  
   parser <- function(series) {
-    
+
     ## check inputs
     assertthat::assert_that(is.character(series))
-    
-    #
-    assertthat::assert_that(all(stringr::str_detect(series, '(@|:)')),
-                            msg = 'No series/database separator')
-    assertthat::assert_that(all(stringr::str_count(series, '(@|:)') == 1),
-                            msg = 'multiple separators detected')
-    
-    if (stringr::str_detect(series, '@')) {
-      
-      s_1 <- 
-        series %>% 
-        stringr::str_to_lower() %>% 
-        stringr::str_split('@') %>% 
-        magrittr::extract2(1)
-      
-      assertthat::assert_that(length(s_1) == 2)
-      
-      series <- stringr::str_c(s_1[2], ':', s_1[1])
-      
-      return(series)  
-      
-    } else if (stringr::str_detect(series, ':')) {
-      series %>% 
-        stringr::str_to_lower()
+
+    assertthat::assert_that(all(stringr::str_detect(series, "(@|:)")),
+                            msg = "No series/database separator")
+    assertthat::assert_that(all(stringr::str_count(series, "(@|:)") == 1),
+                            msg = "multiple separators detected")
+
+    if (stringr::str_detect(series, "@")) {
+
+      s_1 <- stringr::str_to_lower(series)
+      s_2 <- stringr::str_split(s_1, "@")
+      s_3 <- s_2[[1]]
+
+      assertthat::assert_that(length(s_3) == 2)
+
+      out_series <- stringr::str_c(s_3[[2]], ":", s_3[[1]])
+
+      return(out_series)
+
+    } else if (stringr::str_detect(series, ":")) {
+      out_series <- stringr::str_to_lower(series)
+      return(out_series)
     }
   }
-  
-  purrr::map_chr(series, parser)
+
+  parser <- Vectorize(parser, USE.NAMES = FALSE)
+  parser(series)
+
 }
-
-
